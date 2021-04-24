01c86551025c85c48551694cbb93aa175085a249
aleks-boyar <aleks150890@gmail.com>
2021-04-24 09:42:23 +0300

    Homework-2 done

---
+++ HW-2/js/some-js-code.js
@@ -0,0 +1,16 @@
+// // Task#1
+// let name = 'Джон';
+// let admin = "Джон";
+// alert(admin);
+//
+// // Task#2
+// let planet ='earth';
+// let currentUser = "John";
+
+// Task#3
+let str ="10";
+console.log(str, typeof str);
+let n = 10;
+console.log(n, typeof n);
+
+
